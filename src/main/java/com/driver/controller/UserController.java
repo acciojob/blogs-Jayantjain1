ae09018d4a7d1544bb5eef4b4334a70244d44057
Jayantjain1 <jjayant511@gmail.com>
2023-01-15 22:20:15 +0530

    first

--- src/main/java/com/driver/controller/UserController.java
+++ src/main/java/com/driver/controller/UserController.java
@@ -11,23 +11,63 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("/create")
     public ResponseEntity<Void> createUser(@RequestBody User user) {
+        userService.createUser(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{userId}")
-    public ResponseEntity<Void> deleteUser(@PathVariable int userId) {
+    public ResponseEntity<Void> deleteUser(@PathVariable("userId") int userId) {
+        userService.deleteUser(userId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/update")
     public ResponseEntity<Void> updateUser(@RequestBody User user) {
+        userService.updateUser(user);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/find/{username}")
-    public ResponseEntity<User> findUserByUsername(@PathVariable String username) {
+    public ResponseEntity<User> findUserByUsername(@PathVariable("username") String username) {
+        User user=userService.findUserByUsername(username);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
+//package com.driver.controller;
+//
+//import com.driver.models.User;
+//import com.driver.services.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/users")
+//public class UserController {
+//
+//    @PostMapping("/create")
+//    public ResponseEntity<Void> createUser(@RequestBody User user) {
+//        return new ResponseEntity<>(HttpStatus.CREATED);
+//    }
+//
+//    @DeleteMapping("/delete/{userId}")
+//    public ResponseEntity<Void> deleteUser(@PathVariable int userId) {
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
+//
+//    @PutMapping("/update")
+//    public ResponseEntity<Void> updateUser(@RequestBody User user) {
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
+//
+//    @GetMapping("/find/{username}")
+//    public ResponseEntity<User> findUserByUsername(@PathVariable String username) {
+//        return new ResponseEntity<>(user, HttpStatus.OK);
+//    }
+//}
